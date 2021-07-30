ca2e4a7558e880cfc5fe32262e850d73c6777bb7
Artem Gladkikh <artem.and.co@gmail.com>
2021-07-30 12:40:39 +0300

    save

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/gorchestrate/pizzaapp
 
 go 1.16
 
+replace github.com/gorchestrate/async => ../async
+replace github.com/gorchestrate/gasync => ../gasync
+
 require (
 	github.com/gorchestrate/async v0.10.1
 	github.com/gorchestrate/gasync v0.1.3
--- main.go
+++ main.go
@@ -16,9 +16,10 @@ func main() {
 		GCloudProjectID:      "async-315408",
 		GCloudLocationID:     "us-central1",
 		GCloudTasksQueueName: "order",
-		BasePublicURL:        "https://pizzaapp-ffs2ro4uxq-uc.a.run.app/",
+		BasePublicURL:        "https://pizzaapp-ffs2ro4uxq-uc.a.run.app",
 		CORS:                 true,
 		Collection:           "workflows",
+		SignSecret:           "{Secret to sign callbacks sent over public API from Google Cloud Tasks}",
 	}, map[string]func() async.WorkflowState{
 		"pizza": func() async.WorkflowState {
 			return &PizzaOrderWorkflow{}
